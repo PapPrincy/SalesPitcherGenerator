c48e84e610fd81bd66866eb7105a81535c15d71b
Preetha Asunth Princy J <129991620+PapPrincy@users.noreply.github.com>
2024-06-26 11:34:13 -0400

    Update main.py

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -46,11 +46,12 @@ async def generate_sales_pitch_and_email_endpoint(request: Request):
      if not url:
          return JSONResponse(content={"error": "Invalid request body"}, status_code=400)
 
-     sales_pitch_and_email = generate_sales_pitch_and_email(url)
+     sales_pitch,email_content = generate_sales_pitch_and_email(url)
 
-     logging.debug(f"Generated sales pitch and email: {sales_pitch_and_email}")
-     return templates.TemplateResponse("output.html", {"request": request, "sales_pitch_and_email": sales_pitch_and_email})
-     #return JSONResponse(content={"sales_pitch_and_email": sales_pitch_and_email})
+     logging.debug(f"Generated sales pitch : {sales_pitch}")
+     logging.debug(f"Generated email content: {email_content}")
+
+     return templates.TemplateResponse("output.html", {"request": request, "sales_pitch": sales_pitch, "email_content": email_content})
 
 def generate_sales_pitch_and_email(url):
      logging.debug(f"Generating sales pitch and email for URL: {url}")
@@ -63,15 +64,22 @@ def generate_sales_pitch_and_email(url):
 
          wrapped_text = textwrap.fill(document.page_content, width=80)
          prompt = (
-         f"Generate an appealing, effective, clear, concise, and professional sales pitch and also generate an email content for lead generation according to provided URL: {url}"
-         )
+            f"Generate an appealing, effective, clear, concise, and professional sales pitch and also generate an email content for lead generation according to provided URL: {url}. "
+            f"Begin with the sales pitch before transitioning to the email content. Use a horizontal line to divide the sales pitch from the email message. Avoid using asterisks and pound signs."
+        )
 
          response = model.generate_content(prompt)
-         text_only_response = re.sub(r'<[^>]*>', '', response.text)
+         text_only_response = re.sub(r'<[^>]*>|[*]', '', response.text)
          text_only_response = text_only_response.replace('\n', '<br>')
+         # Separate the sales pitch and email content
+         parts = text_only_response.split('---')
+         sales_pitch = parts[0].replace("Sales Pitch:", "").strip()
+         email_content = parts[1].replace("Email Content:", "").strip() if len(parts) > 1 else ""
 
-         logging.debug(f"Generated sales pitch and email: {text_only_response}")
-         return text_only_response
+         logging.debug(f"Generated sales pitch: {sales_pitch}")
+         logging.debug(f"Generated email content: {email_content}")
+         return sales_pitch, email_content
+         
      except Exception as e:
          logging.error(f"Error generating sales pitch and email: {e}")
          return "Error generating sales pitch and email"
